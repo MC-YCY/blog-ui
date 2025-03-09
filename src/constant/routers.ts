4893c7c9bd915bf147c3501078f963828b35ce19
尹春阳 <2646403766@qq.com>
2025-03-09 21:07:08 +0800

    feat: router 常量ts类型

--- src/constant/routers.ts
+++ src/constant/routers.ts
@@ -1,29 +1,39 @@
 import RootLayout from '@/views/layout/index'
 import Home from '@/views/home'
 import Login from '@/views/auth/login'
+import { JSX } from 'react'
 
-export const Routers = [
+interface MetaRouteObject {
+  meta?: {
+    title: string;
+  };
+  path: string;
+  element: () => JSX.Element;
+  children?: MetaRouteObject[]; // 递归定义子路由
+}
+
+export const Routers: MetaRouteObject[] = [
   {
     path: '/',
-    meta:{
-      title:''
+    meta: {
+      title: '',
     },
     element: RootLayout,
     children: [
       {
-        path:'home',
-        meta:{
-          title:'首页'
+        path: 'home',
+        meta: {
+          title: '首页',
         },
         element: Home,
-      }
-    ]
+      },
+    ],
   },
   {
     path: '/login',
     meta: {
-      title:'登录',
+      title: '登录',
     },
     element: Login,
-  }
+  },
 ]
