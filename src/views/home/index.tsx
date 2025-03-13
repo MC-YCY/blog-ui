f8f4fa0b30ad740739955e7bac6d312cf74e8b6a
尹春阳 <2646403766@qq.com>
2025-03-13 19:50:08 +0800

    feat: 删掉link-preview

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,4 @@
 import Hero from './components/hero/index.tsx'
-import { LinkPreviewDemo } from '@/views/home/components/link-preview/link-preview.tsx'
 import TextWords from '@/views/home/components/text-words'
 
 const Home = () => {
@@ -7,7 +6,6 @@ const Home = () => {
     <>
       <Hero>
         <div>
-          <LinkPreviewDemo></LinkPreviewDemo>
           <TextWords></TextWords>
         </div>
       </Hero>
