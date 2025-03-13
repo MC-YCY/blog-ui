ca90b21395af063f3de584101a984e074398bca4
尹春阳 <2646403766@qq.com>
2025-03-13 19:59:57 +0800

    feat: userBtnListProvider

--- src/provider/UserBtnList.provider.tsx
+++ src/provider/UserBtnList.provider.tsx
@@ -4,11 +4,13 @@ import { getPermissionsApi } from '@/api/auth.api.ts'
 
 // 用来获取 用户 权限按钮的
 const UserBtnListProvider = () => {
-  const { setBtnList } = useUserStore()
+  const { setBtnList, logout } = useUserStore()
   const [flat,setFlat] = useState<boolean>(false)
   const getPermissions = () => {
     getPermissionsApi().then(res => {
       setBtnList(res.btnList)
+    }).catch(()=>{
+      logout();
     })
     setFlat(true)
   }
