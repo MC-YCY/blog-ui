59ff65a55e5f3f8d40e722f3ccb8caffb56afc14
尹春阳 <2646403766@qq.com>
2025-03-20 19:25:02 +0800

    feat: 用户信息页面，更新用户信息

--- src/views/user/components/user-info/user-info.tsx
+++ src/views/user/components/user-info/user-info.tsx
@@ -72,60 +72,58 @@ export default function() {
     }
   }
 
-  return <div className={'flex justify-center flex-col items-center py-10 pr-6'}>
-    <div>
-      <div className="space-y-1">
-        <img className={'w-14 h-14 object-cover'} onClick={clickAvatar} src={user?.avatar} alt="" />
-        <input type="file" style={{ display: 'none' }} ref={fileInputRef} onChange={fileInputChange} />
-        <h4 className="text-sm font-medium leading-none">{user?.username}</h4>
-        <p className="text-sm text-muted-foreground">{user?.signature || '知其然不知其所以然'}</p>
-      </div>
-      <Separator className="my-4" />
-      <div className="flex h-5 items-center space-x-4 text-sm">
-        <div className={'cursor-pointer'}>粉丝</div>
-        <Separator orientation="vertical" />
-        <Popover>
-          <PopoverTrigger asChild>
-            <div className={'cursor-pointer'} ref={userEditEl} onClick={clickEditUserInfo}>编辑</div>
-          </PopoverTrigger>
-          <PopoverContent className="w-80 z-1000000">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem className={'bg-transparent dark:bg-transparent'}>
-                      <FormLabel>用户名</FormLabel>
-                      <FormControl>
-                        <Input className={'bg-transparent dark:bg-transparent'} placeholder="请输入用户名" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="signature"
-                  render={({ field }) => (
-                    <FormItem className={'bg-transparent dark:bg-transparent'}>
-                      <FormLabel>个性签名</FormLabel>
-                      <FormControl>
-                        <Textarea className={'bg-transparent dark:bg-transparent'}
-                                  placeholder="请输入个性签名" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="flex justify-center">
-                  <Button type="submit">修改</Button>
-                </div>
-              </form>
-            </Form>
-          </PopoverContent>
-        </Popover>
-      </div>
+  return <div className={'py-10 pr-6 w-full px-2 pb-0'}>
+    <div className="space-y-1">
+      <img className={'w-14 h-14 object-cover'} onClick={clickAvatar} src={user?.avatar} alt="" />
+      <input type="file" style={{ display: 'none' }} ref={fileInputRef} onChange={fileInputChange} />
+      <h4 className="text-sm font-medium leading-none">{user?.username}</h4>
+      <p className="text-sm text-muted-foreground">{user?.signature || '知其然不知其所以然'}</p>
+    </div>
+    <Separator className="my-4" />
+    <div className="flex h-5 items-center space-x-4 text-sm">
+      <div className={'cursor-pointer'}>粉丝</div>
+      <Separator orientation="vertical" />
+      <Popover>
+        <PopoverTrigger asChild>
+          <div className={'cursor-pointer'} ref={userEditEl} onClick={clickEditUserInfo}>编辑</div>
+        </PopoverTrigger>
+        <PopoverContent className="w-80 z-1000000">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className={'bg-transparent dark:bg-transparent'}>
+                    <FormLabel>用户名</FormLabel>
+                    <FormControl>
+                      <Input className={'bg-transparent dark:bg-transparent'} placeholder="请输入用户名" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="signature"
+                render={({ field }) => (
+                  <FormItem className={'bg-transparent dark:bg-transparent'}>
+                    <FormLabel>个性签名</FormLabel>
+                    <FormControl>
+                      <Textarea className={'bg-transparent dark:bg-transparent'}
+                                placeholder="请输入个性签名" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-center">
+                <Button type="submit">修改</Button>
+              </div>
+            </form>
+          </Form>
+        </PopoverContent>
+      </Popover>
     </div>
   </div>
 }
