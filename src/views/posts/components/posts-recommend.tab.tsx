7e5a110e32b310f89068f7dfb8475e059ccdaae5
尹春阳 <2646403766@qq.com>
2025-03-26 21:53:13 +0800

    fix: 文章列表

--- src/views/posts/components/posts-recommend.tab.tsx
+++ src/views/posts/components/posts-recommend.tab.tsx
@@ -1,10 +1,16 @@
 import { ArticleCard } from '@/components/ui/article-card.tsx'
 import { useEffect, useState } from 'react'
 import { allArticlesList } from '@/api/article.api.ts'
+import { SmartPagination } from '@/components/ui/pagination-controller.tsx'
 
 export default function() {
   const [list, setList] = useState([])
   const [total, setTotal] = useState<number>(0)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(10)
+  const onChange = (p: number) => {
+    setCurrentPage(p)
+  }
   const getList = () => {
     let params = {
       page: 1,
@@ -18,11 +24,20 @@ export default function() {
   }
   useEffect(() => {
     getList()
-  }, [])
+  }, [currentPage])
   return <div>
+    <div className={'fixed bottom-10 flex justify-center w-full left-0'}>
+      <SmartPagination
+        current={currentPage}
+        total={100}
+        pageSize={pageSize}
+        onChange={onChange}
+      />
+    </div>
     {
       list.map(item => {
         return <ArticleCard
+          key={item.id}
           title={item.title}
           excerpt={item.readme}
           date={item.createdAt}
@@ -34,6 +49,6 @@ export default function() {
         />
       })
     }
-
+    <div className={'h-20'}></div>
   </div>
 }
