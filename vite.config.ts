5cd2a6b44dc1039c787354239e1b84a45270e510
尹春阳 <2646403766@qq.com>
2025-04-02 22:56:56 +0800

    fix: 打包部署的错误

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,8 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
+      host: '0.0.0.0', // 监听所有网络接口（包括局域网 IP）
+      port: 5173,      // 可选：指定端口
       proxy: {
         [env.VITE_APP_API_PREFIX]: {
           target: env.VITE_APP_BASE_API,
@@ -34,37 +36,37 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {  // 改用函数形式精准控制分包
-            // 按功能模块分包
-            if (id.includes('/docs/sa-calendar-')) {
-              return 'calendar'; // sa-calendar 相关组件独立分包
-            }
-            if (id.includes('/user/')) {
-              return 'user'; // 用户相关页面独立分包
-            }
-            if (id.includes('/create') || id.includes('/update')) {
-              return 'editor'; // 创作/编辑页面独立分包
-            }
+    // build: {
+    //   rollupOptions: {
+    //     output: {
+    //       manualChunks(id) {  // 改用函数形式精准控制分包
+    //         // 按功能模块分包
+    //         if (id.includes('/docs/sa-calendar-')) {
+    //           return 'calendar'; // sa-calendar 相关组件独立分包
+    //         }
+    //         if (id.includes('/user/')) {
+    //           return 'user'; // 用户相关页面独立分包
+    //         }
+    //         if (id.includes('/create') || id.includes('/update')) {
+    //           return 'editor'; // 创作/编辑页面独立分包
+    //         }
 
-            // 第三方依赖分包（避免重复打包）
-            if (id.includes('node_modules')) {
-              if (id.includes('react')) {
-                return 'vendor-react';
-              }
-              if (id.includes('antd')) {
-                return 'vendor-antd';
-              }
-              if (id.includes('lodash')) {
-                return 'vendor-lodash';
-              }
-              return 'vendor-other';
-            }
-          }
-        }
-      }
-    }
+    //         // 第三方依赖分包（避免重复打包）
+    //         if (id.includes('node_modules')) {
+    //           if (id.includes('react')) {
+    //             return 'vendor-react';
+    //           }
+    //           if (id.includes('antd')) {
+    //             return 'vendor-antd';
+    //           }
+    //           if (id.includes('lodash')) {
+    //             return 'vendor-lodash';
+    //           }
+    //           return 'vendor-other';
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   };
 });
