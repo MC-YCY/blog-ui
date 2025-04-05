dffffd7d76e5f56ceaf69ca7b25623f6e43a0616
尹春阳 <2646403766@qq.com>
2025-04-05 22:23:05 +0800

    feat:生产环境ws

--- src/views/layout/components/header/components/message-button.tsx
+++ src/views/layout/components/header/components/message-button.tsx
@@ -29,7 +29,10 @@ export default function NotificationBell() {
     if (!user?.id) return
 
     // 初始化 Socket 连接
-    socketRef.current = io('http://localhost:3000', {
+    const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+    const wsUrl = `${protocol}://${window.location.host}`;
+    socketRef.current = io(wsUrl, {
+      path: '/ws', // 与后端 path 配置一致
       query: { userId: user.id.toString() }, // 必须字符串类型
       transports: ['websocket'],
       reconnection: true,
