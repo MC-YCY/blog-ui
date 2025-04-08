480dc08a232b6ade3d79302ebff124a7882942a2
尹春阳 <2646403766@qq.com>
2025-04-08 22:56:08 +0800

    fix: 更改UserBtnListProvider获取权限时机

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import ThemeProvider from '@/provider/Theme.provider.tsx'
 import useThemeStore from '@/stores/themeStore.ts'
 import PlayRouter from '@/components/router.tsx'
 import { Toaster } from 'sonner'
+import UserBtnListProvider from '@/provider/UserBtnList.provider.tsx'
 
 function App() {
   // 初始主题store缓存
   useThemeStore.persist.rehydrate()
 
   return (<>
+    <UserBtnListProvider></UserBtnListProvider>
     <ThemeProvider></ThemeProvider>
     <PlayRouter></PlayRouter>
     <Toaster></Toaster>
--- src/views/layout/index.tsx
+++ src/views/layout/index.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/header/index.tsx';
 import WrapperBackground from '@/components/wrapper-background/wrapper-background.tsx'
-import UserBtnListProvider from '@/provider/UserBtnList.provider.tsx'
 
 const Layout = () => {
   return (
     <>
-      <UserBtnListProvider></UserBtnListProvider>
       <WrapperBackground></WrapperBackground>
       <Header></Header>
       <Outlet></Outlet>
