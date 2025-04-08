51586526c160d6d9dc68598e342a926eca8371b5
尹春阳 <2646403766@qq.com>
2025-04-08 22:53:17 +0800

    fix: 浏览器插件导致的html高度固定

--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -47,7 +47,7 @@ export default function({ info }: { info?: ArticleItem } = { info: undefined }) 
       }
     }
   }
-  return <div className={'w-screen h-screen'}>
+  return <div className={'w-full h-screen'}>
     <Header defaultInfo={info} submit={onSubmit}></Header>
     <div className={'h-[calc(100vh-72px)] overflow-hidden'}>
       <EditorMarkdown defaultInfo={info} ref={mdEditorRef}></EditorMarkdown>
