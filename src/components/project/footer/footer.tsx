4a0ee71f85866a027e0873a37664072e19ef030c
春秋半夏 <yin_chunyang@163.com>
2025-05-30 17:37:58 +0800

    feat: footer

--- src/components/project/footer/footer.tsx
+++ src/components/project/footer/footer.tsx
@@ -1,21 +1,18 @@
 'use client';
 
-import {IconBrandGithub, IconBrandNextjs, IconBrandReact, IconBrandTailwind} from "@tabler/icons-react";
+import {IconBrandGithub, IconBrandReact, IconBrandTailwind} from "@tabler/icons-react";
 
 export const Footer = () => {
     return <div className={'bg-[#000] mt-20'}>
         <div className={'max-w-[100rem] mx-auto py-10 px-2 md:px-8'}>
             <div className={'flex gap-10 flex-wrap'}>
-                <a className={'text-[#fff] cursor-pointer'} target={'_blank'} href={'https://nextjs.org/'}>
-                    <IconBrandNextjs></IconBrandNextjs>
-                </a>
                 <a className={'text-[#fff] cursor-pointer'} target={'_blank'} href={'https://react.dev/'}>
                     <IconBrandReact></IconBrandReact>
                 </a>
                 <a className={'text-[#fff] cursor-pointer'} target={'_blank'} href={'https://tailwindcss.com/'}>
                     <IconBrandTailwind></IconBrandTailwind>
                 </a>
-                <a className={'text-[#fff] cursor-pointer'} target='_blank' rel='noreferrer' href={'https://github.com/MC-YCY/next-blog'}>
+                <a className={'text-[#fff] cursor-pointer'} target='_blank' rel='noreferrer' href={'https://github.com/MC-YCY/blog-ui/tree/next-blog-ui'}>
                     <IconBrandGithub></IconBrandGithub>
                 </a>
             </div>
