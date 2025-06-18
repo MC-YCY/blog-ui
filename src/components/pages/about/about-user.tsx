8b82d6820de555f23ede80559a4ee2dc317fe7b3
春秋半夏 <yin_chunyang@163.com>
2025-06-18 15:46:32 +0800

    style:about

--- src/components/pages/about/about-user.tsx
+++ src/components/pages/about/about-user.tsx
@@ -2,31 +2,32 @@ import UserImg from '@/assets/images/me/user.jpg'
 import style from './style.module.css'
 import { cn } from '@/lib/utils.ts'
 
-const styleClassName = 'px-3 py-0.5 bg-background border rounded-2xl shadow-sm cursor-default'
+// const styleClassName = 'px-3 py-0.5 bg-background border rounded-2xl shadow-sm cursor-default'
 export const AboutUser = () => {
-  return <div className={'w-full flex justify-center pb-[40px] pt-[20px]'}>
-    <div className={'hidden md:flex flex-col justify-around py-[10px] text-[14px]'}>
-      <div className={cn('ml-[16px] ', styleClassName, style.ani)}>
-        🤖️ 数码科技爱好者
-      </div>
-      <div className={cn('mr-[16px]', styleClassName, style.ani)}>🔍 分享与热心帮助</div>
-      <div className={cn('mr-[16px]', styleClassName, style.ani)}>🏠 智能家居小能手</div>
-      <div className={cn('ml-[16px] ', styleClassName, style.ani)}>
-        🔨 吹牛开发一条龙
-      </div>
-    </div>
-    <div className={cn('w-[180px] h-[180px] rounded-[180px] mx-6 cursor-pointer hover:scale-110 hover:shadow-xl hover:rotate-360 transition',style.ani)}>
+  return <div className={'w-full flex justify-center pb-[70px] pt-[20px]'}>
+    {/*<div className={'hidden md:flex flex-col justify-around py-[10px] text-[14px]'}>*/}
+    {/*  <div className={cn('ml-[16px] ', styleClassName, style.ani)}>*/}
+    {/*    🤖️ 数码科技爱好者*/}
+    {/*  </div>*/}
+    {/*  <div className={cn('mr-[16px]', styleClassName, style.ani)}>🔍 分享与热心帮助</div>*/}
+    {/*  <div className={cn('mr-[16px]', styleClassName, style.ani)}>🏠 智能家居小能手</div>*/}
+    {/*  <div className={cn('ml-[16px] ', styleClassName, style.ani)}>*/}
+    {/*    🔨 吹牛开发一条龙*/}
+    {/*  </div>*/}
+    {/*</div>*/}
+    <div
+      className={cn('w-[180px] h-[180px] rounded-[180px] mx-6 cursor-pointer hover:scale-110 hover:shadow-xl hover:rotate-360 transition', style.ani)}>
       <img className={'w-full h-full object-cover rounded-[180px]'} src={UserImg} alt="" />
     </div>
-    <div className={'hidden md:flex flex-col justify-around py-[10px] text-[14px]'}>
-      <div className={cn('mr-[16px]', styleClassName, style.ani)}>分享与热心帮助 🤖️</div>
-      <div className={cn('ml-[16px] ', styleClassName, style.ani)}>分享与热心帮助
-        🔍
-      </div>
-      <div className={cn('ml-[16px] ', styleClassName, style.ani)}>分享与热心帮助
-        🔍
-      </div>
-      <div className={cn('mr-[16px]', styleClassName, style.ani)}>分享与热心帮助 🔨</div>
-    </div>
+    {/*<div className={'hidden md:flex flex-col justify-around py-[10px] text-[14px]'}>*/}
+    {/*  <div className={cn('mr-[16px]', styleClassName, style.ani)}>分享与热心帮助 🤖️</div>*/}
+    {/*  <div className={cn('ml-[16px] ', styleClassName, style.ani)}>分享与热心帮助*/}
+    {/*    🔍*/}
+    {/*  </div>*/}
+    {/*  <div className={cn('ml-[16px] ', styleClassName, style.ani)}>分享与热心帮助*/}
+    {/*    🔍*/}
+    {/*  </div>*/}
+    {/*  <div className={cn('mr-[16px]', styleClassName, style.ani)}>分享与热心帮助 🔨</div>*/}
+    {/*</div>*/}
   </div>
 }
--- src/views/about/page.tsx
+++ src/views/about/page.tsx
@@ -11,7 +11,6 @@ const AboutPage = () => {
   return <div className={'pt-[64px]'}>
     <Container className={'max-w-[1400px]'}>
       <AboutUser></AboutUser>
-      <div className={'text-[28px] text-center mt-[20px] mb-[40px] font-bold'}>关于本站</div>
       <AboutIntroduce></AboutIntroduce>
       <AboutSkill></AboutSkill>
       <AboutPersonality></AboutPersonality>
