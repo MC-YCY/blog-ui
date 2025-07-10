6042c01b7fd3941997bf79f12a0d3e7e3bf6d48e
春秋半夏 <yin_chunyang@163.com>
2025-07-10 14:42:08 +0800

    feat:layout MusicPlayer fixed--> sticky

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
       <div className={'min-h-[calc(100vh-64px)] max-w-screen'}>
         <Outlet></Outlet>
       </div>
-      <MusicPlayer className={'sticky left-[20px] bottom-[20px] z-[50] border-[#e3e8f7] dark:border-[#3d3d3f] border-solid border shadow-[0_0_10px_rgba(0,0,0,0.05)] dark:shadow-[0_0_8px_00000050]'}></MusicPlayer>
+      <MusicPlayer className={'sticky left-[20px] bottom-[20px] z-[50] mt-[60px] border-[#e3e8f7] dark:border-[#3d3d3f] border-solid border shadow-[0_0_10px_rgba(0,0,0,0.05)] dark:shadow-[0_0_8px_00000050]'}></MusicPlayer>
       <Footer></Footer>
     </>
   )
