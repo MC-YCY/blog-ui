94b92f982567905317be0258d9ca2cbab7f23275
春秋半夏 <yin_chunyang@163.com>
2025-08-14 18:00:57 +0800

    feat：@/assets/images/share.png

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -3,6 +3,7 @@ import { Footer } from '@/components/project/footer/footer'
 import { Outlet } from 'react-router-dom'
 import { MusicPlayer } from '@/components/project/music/player.tsx'
 import { MusicConst } from '@/constant/music-const.ts'
+import ShareBanner from '@/assets/images/share.png'
 
 const Layout = () => {
   return (
@@ -14,6 +15,7 @@ const Layout = () => {
       <MusicPlayer musicList={MusicConst}
                    className={'sticky left-[20px] bottom-[20px] z-[50] mt-[60px] shadow-[0_0_10px_rgba(0,0,0,0.05)] dark:shadow-[0_0_8px_00000050]'}></MusicPlayer>
       <Footer></Footer>
+      <img src={ShareBanner} className={'w-screen h-screen fixed left-0 top-0 z-[-1] pointer-none'} alt="" />
     </>
   )
 }
